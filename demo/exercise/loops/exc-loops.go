aea1bfe5db6d3511d420f1e161a1d465f82766af
thaisc98 <thaisc98@hotmail.es>
2025-12-18 17:53:58 -0400

    [ADD] loops

---
+++ demo/exercise/loops/exc-loops.go
@@ -0,0 +1,35 @@
+//--Summary:
+//  Implement the classic "FizzBuzz" problem using a `for` loop.
+//
+//--Requirements:
+//* Print integers 1 to 50, except:
+//  - Print "Fizz" if the integer is divisible by 3
+//  - Print "Buzz" if the integer is divisible by 5
+//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+//
+//--Notes:
+//* The remainder operator (%) can be used to determine divisibility
+
+package main
+
+import "fmt"
+func main() {
+
+	for i:= 1; i <= 50; i++ {
+		divisibleBy3 := i % 3 == 0
+		dividibleBy5 := i % 5 == 0
+
+		if divisibleBy3 && dividibleBy5{
+			fmt.Println("FizzBuzz")
+		} else if divisibleBy3 {
+			fmt.Println("Fizz")
+		} else if dividibleBy5 {
+			fmt.Println("Buzz")
+		}else{
+			fmt.Println(i)
+		}
+
+	}
+
+}
+
