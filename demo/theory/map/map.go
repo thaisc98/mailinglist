d2f5d55638933497bc6d007c2ee911ef4512fd34
thaisc98 <thaisc98@hotmail.es>
2025-12-28 18:37:06 -0400

    [FIX] map theory

---
+++ demo/theory/map/map.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main(){
+	shoppingList := make(map[string]int)
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 1
+	shoppingList["bread"] += 1
+
+	shoppingList["eggs"] += 1
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+	fmt.Println("Milk deleted, new list: ", shoppingList)
+
+	fmt.Println("need", shoppingList["eggs"], "eggs")
+
+	cereal, found := shoppingList["cereal"]
+	fmt.Println("Need ceral?")
+
+	if !found {
+		fmt.Println("nope")
+	} else {
+		fmt.Println("yupe", cereal)
+	}
+
+	totalItems := 0
+
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+
+	fmt.Println("There are", totalItems,  "on the shopping list")
+
+}
