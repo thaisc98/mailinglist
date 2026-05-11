07a0f69c179b87596ca6dedf01717f6b522e0119
thaisc98 <thaisc98@hotmail.es>
2026-05-11 18:55:03 -0400

    grpc apis

--- go.mod
+++ go.mod
@@ -5,6 +5,14 @@ go 1.25.5
 require (
 	github.com/alexflint/go-arg v1.6.1
 	github.com/mattn/go-sqlite3 v1.14.44
+	google.golang.org/grpc v1.81.0
+	google.golang.org/protobuf v1.36.11
 )
 
-require github.com/alexflint/go-scalar v1.2.0 // indirect
+require (
+	github.com/alexflint/go-scalar v1.2.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171 // indirect
+)
--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"mailinglist/grpcapi"
 	"mailinglist/jsonapi"
 	"mailinglist/mdb"
 	"sync"
@@ -13,6 +14,7 @@ import (
 var args struct {
 	DbPath string `arg:"env:MAILINGLIST_DB"`
 	BindJson string `arg:"env:MAILINGLIST_BIND_JSON"`
+	BindGrpc string `arg:"env:MAILINGLIST_BIND_GRPC"`
 }
 
 func main () {
@@ -22,6 +24,9 @@ func main () {
 	}
 	if args.BindJson == "" {
 		args.BindJson = ":8080"
+	}
+	if args.BindGrpc == "" {
+		args.BindGrpc = ":8081"
 	}
 
 	log.Printf("Using dabase '%v' ", args.DbPath)
@@ -39,6 +44,12 @@ func main () {
 	go func() {
 		log.Printf("Starting JSON API server...\n")
 		jsonapi.Serve(db, args.BindJson)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		log.Printf("Starting gRPC API server...\n")
+		grpcapi.Serve(db, args.BindGrpc)
 		wg.Done()
 	}()
 
